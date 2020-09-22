e40025a22b61d03a0acf7130a0e542bbeb425507
Sacha Bartholme <sacha.bartholme@hotmail.com>
2020-09-22 23:47:34 +0200

    implemented Run and Stop

--- gossip/gossiper.go
+++ gossip/gossiper.go
@@ -6,7 +6,8 @@ package gossip
 import (
 	"go.dedis.ch/onet/v3/log"
 	"reflect"
-	"golang.org/x/xerrors"
+	"net"
+	"fmt"
 )
 
 // BaseGossipFactory provides a factory to instantiate a Gossiper
@@ -29,7 +30,10 @@ func (f BaseGossipFactory) New(address, identifier string) (BaseGossiper, error)
 // - implements gossip.BaseGossiper
 type Gossiper struct {
 	Handlers map[reflect.Type]interface{}
-	// *** Add more here
+	address string
+	identifier string
+	conn *net.UDPConn
+	udpAddr *net.UDPAddr
 }
 
 // NewGossiper returns a Gossiper that is able to listen to the given address
@@ -37,16 +41,19 @@ type Gossiper struct {
 // address. To run the gossip protocol and create the UDP connection, call `Run`
 // on the gossiper.
 func NewGossiper(address, identifier string) (BaseGossiper, error) {
-	log.Error("Implement me")
 
-	// TODO define a gossiper g, then enable the following code
-	/*
+	g := Gossiper{
+		Handlers: make(map[reflect.Type]interface{}),
+		address: address,
+		identifier: identifier,
+	}
+
 	err := g.RegisterHandler(&SimpleMessage{})
 	if err != nil {
 		log.Fatal("failed to register", err)
 	}
-	*/
-	return nil, xerrors.New("implement me")
+	
+	return &g, nil
 }
 
 // Run implements gossip.BaseGossiper. It creates the UDP connection and starts
@@ -54,7 +61,30 @@ func NewGossiper(address, identifier string) (BaseGossiper, error) {
 // This method can panic if it is not possible to create the connection on that
 // address. The ready chan must be closed when it is running.
 func (g *Gossiper) Run(ready chan struct{}) {
-	log.Error("Implement me")
+
+	var err error
+	g.udpAddr, err = net.ResolveUDPAddr("udp4", g.address)
+	if err != nil {
+		panic(fmt.Sprintf("Could not resolve UDP addr: %v", err))
+	}
+
+	g.conn, err = net.ListenUDP("udp4", g.udpAddr)
+	if err != nil {
+		panic(fmt.Sprintf("Could not listen to UDP addr: %v", err))
+	}
+
+	ready <- struct{}{}
+
+	buffer := make([]byte, 1024)
+	for {
+
+		n, client_addr, err := g.conn.ReadFromUDP(buffer)
+		if err != nil {
+			panic(fmt.Sprintf("Could not read from UDP addr: %v", err))
+		}
+
+		fmt.Println("Received UDP packet: %v from %v", string(buffer[0:n-1]), client_addr)
+	}
 }
 
 // Stop implements gossip.BaseGossiper. It closes the UDP connection. You should
@@ -62,7 +92,12 @@ func (g *Gossiper) Run(ready chan struct{}) {
 // you can send a stop message to the listener, which then knows that it can
 // stop listening.
 func (g *Gossiper) Stop() {
-	log.Error("Implement me")
+	
+	_, err := g.conn.WriteToUDP([]byte(stopMsg), g.udpAddr)
+	if err != nil {
+		panic(fmt.Sprintf("Could not write to UDP addr: %v", err))
+	}
+	g.conn.Close()
 }
 
 // AddSimpleMessage implements gossip.BaseGossiper. It takes a text that will be
